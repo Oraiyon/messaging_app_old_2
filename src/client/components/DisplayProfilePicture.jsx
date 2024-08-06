0c67188b945826603e7b4914263a465ade18c481
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-08-06 13:04:25 +0800

    Refactored <DisplayProfilePicture />. Moved function that decided whether to use an img or a svg into new function.

--- src/client/components/DisplayProfilePicture.jsx
+++ src/client/components/DisplayProfilePicture.jsx
@@ -20,55 +20,45 @@ const DisplayProfilePicture = (props) => {
     props.formRef.current.click();
   };
 
-  // Make profile.picture default to default pic?
-  if (props.profile.picture) {
-    return (
-      <>
-        <div
-          className={props.user ? "user_picture_container" : styles.profile_picture_container}
-          onClick={openPictureForm}
-          onMouseOver={() => displayEditPictureRef(editPictureIconRef)}
-          onMouseOut={() => displayEditPictureRef(editPictureIconRef)}
-        >
-          <img
-            src={props.profile.picture}
-            alt="Profile Picture"
-            title={props.user ? "Change Profile Picture" : ""}
+  const ProfilePicturePicker = (props) => {
+    if (props.profile.picture) {
+      return (
+        <img
+          src={props.profile.picture}
+          alt="Profile Picture"
+          title={props.user ? "Change Profile Picture" : ""}
+          className={styles.profile_picture}
+        />
+      );
+    } else {
+      return (
+        <div className={styles.default_profile_picture}>
+          <Icon
+            path={mdiAccountCircle}
             className={styles.profile_picture}
-          />
-          <Icon
-            path={mdiPencilCircleOutline}
-            className={styles.edit_picture_icon}
-            ref={editPictureIconRef}
+            title="Change Profile Picture"
           />
         </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div
-          className={props.user ? "user_picture_container" : styles.profile_picture_container}
-          onClick={openPictureForm}
-          onMouseOver={() => displayEditPictureRef(editPictureIconRef)}
-          onMouseOut={() => displayEditPictureRef(editPictureIconRef)}
-        >
-          <div className={styles.default_profile_picture}>
-            <Icon
-              path={mdiAccountCircle}
-              className={styles.profile_picture}
-              title="Change Profile Picture"
-            />
-          </div>
-          <Icon
-            path={mdiPencilCircleOutline}
-            className={styles.edit_picture_icon}
-            ref={editPictureIconRef}
-          />
-        </div>
-      </>
-    );
-  }
+      );
+    }
+  };
+
+  // Make profile.picture default to default pic?
+  return (
+    <div
+      className={props.user ? "user_picture_container" : styles.profile_picture_container}
+      onClick={openPictureForm}
+      onMouseOver={() => displayEditPictureRef(editPictureIconRef)}
+      onMouseOut={() => displayEditPictureRef(editPictureIconRef)}
+    >
+      <ProfilePicturePicker profile={props.profile} />
+      <Icon
+        path={mdiPencilCircleOutline}
+        className={styles.edit_picture_icon}
+        ref={editPictureIconRef}
+      />
+    </div>
+  );
 };
 
 export default DisplayProfilePicture;
